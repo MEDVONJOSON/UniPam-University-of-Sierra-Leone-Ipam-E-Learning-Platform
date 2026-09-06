416f3aace6dd12ddc1cf3cc1dcd903e1abb5a2ef
medvo <medvo@example.com>
2026-09-06 03:21:19 +0000

    fix: import useEffect in CourseCatalogPage

--- client/src/pages/learning/CourseCatalogPage.jsx
+++ client/src/pages/learning/CourseCatalogPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getCurrentUser } from "../../services/authService";
 import { Link } from "react-router-dom";
 import {
